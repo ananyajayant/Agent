d73dae37946402162542fd01d3970b6ecf732efe
Ananya Budhauliya <87610762+ananyajayant@users.noreply.github.com>
2023-10-30 04:04:41 +0530

    segment.py
    Used PyDub to split the audio into segments that belong to the caller and callee.

--- segment.py
+++ segment.py
@@ -2,7 +2,7 @@ from pydub import AudioSegment
 from pydub.playback import play
 
 # Load the audio file
-audio = AudioSegment.from_file("conversation.mp3")
+audio = AudioSegment.from_file("storage.mp3")
 
 # Split the audio into segments of 10 seconds (adjust as needed)
 segment_duration = 10 * 1000  # 10 seconds in milliseconds
